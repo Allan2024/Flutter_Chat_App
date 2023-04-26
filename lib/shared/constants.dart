bf557fcfb9c1a662342bb788319b10d5ccd7ae93
Allan2024 <allanjoseph009@gmail.com>
2023-04-26 11:27:35 +0530

    first

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:230651624902:web:22aa0fac6852daa7dcc6c7";
+  static String apiKey = "AIzaSyAyffyWUCFwHhKppyhQK5egOQbF-dvriP4";
+  static String messagingSenderId = "230651624902";
+  static String projectId = "chatappflutter-b9300";
+  final primaryColor = const Color(0xFFee7b64);
+}
